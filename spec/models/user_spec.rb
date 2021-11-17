2309399ea84fe5f412e00a314e090c6c12e30117
jenobpj <jenobgit@gmail.com>
2021-11-17 12:05:38 -0500

    Add test to user_spec:User not valid when password is shorter than 5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to include ("Password can't be blank")
     end
-    
+
     it "is not valid without pasword confirmation" do
       subject.password_confirmation = nil
       expect(subject).to_not be_valid
@@ -56,6 +56,20 @@ RSpec.describe User, type: :model do
       same_as_subject = User.create(first_name: "Test", last_name: "Person", email: "jenob@gmail.com", password: "secret", password_confirmation: "secret")
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to include ("Email has already been taken")
+    end
+
+    it "is not valid when password is shorter than 5 characters" do
+      subject.password = "abcd"
+      subject.password_confirmation = "abcd"
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include ("Password is too short (minimum is 5 characters)")
+    end
+
+    it "is valid when password is exactly 5 characters" do
+      subject.password = "abcde"
+      subject.password_confirmation = "abcde"
+      expect(subject).to be_valid
+      expect(subject.errors.full_messages).to be_empty
     end
   end
 end
