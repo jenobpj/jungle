0dcf385ca52be9532f23fda6ad00b13194132675
jenobpj <jenobgit@gmail.com>
2021-11-17 11:51:33 -0500

    Refactor tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe User, type: :model do
       first_name: "Jenob", 
       last_name: "Job", 
       email: "jenob@gmail.com", 
-      password: "nice", 
-      password_confirmation: "nice"
+      password: "secret", 
+      password_confirmation: "secret"
     )
   }
 
@@ -17,19 +17,18 @@ RSpec.describe User, type: :model do
       expect(subject.errors.full_messages).to be_empty
     end
 
-    it "is not valid without first name" do
+      it "is not valid without first name" do
       subject.first_name = nil
       expect(subject).to_not be_valid
-      expect(user.errors.full_messages).to include ("First name can't be blank")
+      expect(subject.errors.full_messages).to include ("First name can't be blank")
     end
-
     it "is not valid when password and password_confirmation don't match" do
       subject.password_confirmation = "notsecret"
-      expect(subject.to_not be_valid
-      expect(user.errors.full_messages).to include ("Password confirmation doesn't match Password")
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include ("Password confirmation doesn't match Password")
     end
     it "is not valid when email isn't unique (case insensitive)" do
-      same_as_subject = User.create(first_name: "Test", last_name: "Person", email: "jenob@gmail.com", password: "nice", password_confirmation: "nice")
+      same_as_subject = User.create(first_name: "Test", last_name: "Person", email: "jenob@gmail.com", password: "secret", password_confirmation: "secret")
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to include ("Email has already been taken")
     end
